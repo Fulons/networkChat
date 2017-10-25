966c3ea0aaba779ae9cd666ce84ca9423917b14c
Fulons <fulons.k@gmail.com>
2017-10-25 14:32:54 +0100

    chatlog design

---
+++ Client/frmChatLog.Designer.vb
@@ -0,0 +1,71 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmChatLog
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.rtxtChatLog = New System.Windows.Forms.RichTextBox()
+        Me.lbUsers = New System.Windows.Forms.ListBox()
+        Me.btnQuit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'rtxtChatLog
+        '
+        Me.rtxtChatLog.Location = New System.Drawing.Point(12, 12)
+        Me.rtxtChatLog.Name = "rtxtChatLog"
+        Me.rtxtChatLog.Size = New System.Drawing.Size(569, 459)
+        Me.rtxtChatLog.TabIndex = 0
+        Me.rtxtChatLog.Text = ""
+        '
+        'lbUsers
+        '
+        Me.lbUsers.FormattingEnabled = True
+        Me.lbUsers.Location = New System.Drawing.Point(587, 12)
+        Me.lbUsers.Name = "lbUsers"
+        Me.lbUsers.Size = New System.Drawing.Size(120, 459)
+        Me.lbUsers.TabIndex = 1
+        '
+        'btnQuit
+        '
+        Me.btnQuit.Location = New System.Drawing.Point(632, 477)
+        Me.btnQuit.Name = "btnQuit"
+        Me.btnQuit.Size = New System.Drawing.Size(75, 23)
+        Me.btnQuit.TabIndex = 2
+        Me.btnQuit.Text = "Quit"
+        Me.btnQuit.UseVisualStyleBackColor = True
+        '
+        'frmChatLog
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(719, 505)
+        Me.Controls.Add(Me.btnQuit)
+        Me.Controls.Add(Me.lbUsers)
+        Me.Controls.Add(Me.rtxtChatLog)
+        Me.Name = "frmChatLog"
+        Me.Text = "frmChatLog"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents rtxtChatLog As System.Windows.Forms.RichTextBox
+    Friend WithEvents lbUsers As System.Windows.Forms.ListBox
+    Friend WithEvents btnQuit As System.Windows.Forms.Button
+End Class
---
+++ Client/frmChatLog.vb
@@ -0,0 +1,3 @@
+﻿Public Class frmChatLog
+
+End Class
