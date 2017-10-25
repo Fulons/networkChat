f87b90fcbc98622a8517dbc7fdf4fb2a58e93868
Fulons <fulons.k@gmail.com>
2017-10-25 14:20:30 +0100

    Restarting redesign

---
+++ Client/frmClient.Designer.vb
@@ -0,0 +1,147 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmClient
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.rtxtChat = New System.Windows.Forms.RichTextBox()
+        Me.lbUsers = New System.Windows.Forms.ListBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.txtMessageInput = New System.Windows.Forms.TextBox()
+        Me.btnSend = New System.Windows.Forms.Button()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuLogin = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuLogout = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuChatLogs = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuHelp = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuExit = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'rtxtChat
+        '
+        Me.rtxtChat.Location = New System.Drawing.Point(12, 27)
+        Me.rtxtChat.Name = "rtxtChat"
+        Me.rtxtChat.Size = New System.Drawing.Size(694, 272)
+        Me.rtxtChat.TabIndex = 0
+        Me.rtxtChat.Text = ""
+        '
+        'lbUsers
+        '
+        Me.lbUsers.FormattingEnabled = True
+        Me.lbUsers.Location = New System.Drawing.Point(712, 27)
+        Me.lbUsers.Name = "lbUsers"
+        Me.lbUsers.Size = New System.Drawing.Size(120, 303)
+        Me.lbUsers.TabIndex = 1
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(844, 24)
+        Me.MenuStrip1.TabIndex = 2
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'txtMessageInput
+        '
+        Me.txtMessageInput.Location = New System.Drawing.Point(12, 309)
+        Me.txtMessageInput.Name = "txtMessageInput"
+        Me.txtMessageInput.Size = New System.Drawing.Size(613, 20)
+        Me.txtMessageInput.TabIndex = 3
+        '
+        'btnSend
+        '
+        Me.btnSend.Location = New System.Drawing.Point(631, 307)
+        Me.btnSend.Name = "btnSend"
+        Me.btnSend.Size = New System.Drawing.Size(75, 23)
+        Me.btnSend.TabIndex = 4
+        Me.btnSend.Text = "Send"
+        Me.btnSend.UseVisualStyleBackColor = True
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuLogin, Me.mnuLogout, Me.mnuChatLogs, Me.mnuHelp, Me.mnuExit})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'mnuLogin
+        '
+        Me.mnuLogin.Name = "mnuLogin"
+        Me.mnuLogin.Size = New System.Drawing.Size(152, 22)
+        Me.mnuLogin.Text = "Login"
+        '
+        'mnuLogout
+        '
+        Me.mnuLogout.Name = "mnuLogout"
+        Me.mnuLogout.Size = New System.Drawing.Size(152, 22)
+        Me.mnuLogout.Text = "Logout"
+        '
+        'mnuChatLogs
+        '
+        Me.mnuChatLogs.Name = "mnuChatLogs"
+        Me.mnuChatLogs.Size = New System.Drawing.Size(152, 22)
+        Me.mnuChatLogs.Text = "Chat Logs"
+        '
+        'mnuHelp
+        '
+        Me.mnuHelp.Name = "mnuHelp"
+        Me.mnuHelp.Size = New System.Drawing.Size(152, 22)
+        Me.mnuHelp.Text = "Help"
+        '
+        'mnuExit
+        '
+        Me.mnuExit.Name = "mnuExit"
+        Me.mnuExit.Size = New System.Drawing.Size(152, 22)
+        Me.mnuExit.Text = "Exit"
+        '
+        'frmClient
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(844, 336)
+        Me.Controls.Add(Me.btnSend)
+        Me.Controls.Add(Me.txtMessageInput)
+        Me.Controls.Add(Me.lbUsers)
+        Me.Controls.Add(Me.rtxtChat)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "frmClient"
+        Me.Text = "frmClient"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents rtxtChat As System.Windows.Forms.RichTextBox
+    Friend WithEvents lbUsers As System.Windows.Forms.ListBox
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents FileToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents mnuLogin As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents mnuLogout As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents mnuChatLogs As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents mnuHelp As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents mnuExit As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents txtMessageInput As System.Windows.Forms.TextBox
+    Friend WithEvents btnSend As System.Windows.Forms.Button
+End Class
