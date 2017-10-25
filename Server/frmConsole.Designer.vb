7ee8becc5f4538d9816eddf006f7a4167e3cda04
Fulons <fulons.k@gmail.com>
2017-10-25 14:44:04 +0100

    frmConsole

---
+++ Server/frmConsole.Designer.vb
@@ -0,0 +1,147 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmConsole
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuSettings = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuConnect = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuDisconnect = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuExit = New System.Windows.Forms.ToolStripMenuItem()
+        Me.rtxtConsole = New System.Windows.Forms.RichTextBox()
+        Me.lbUsers = New System.Windows.Forms.ListBox()
+        Me.txtConsoleInput = New System.Windows.Forms.TextBox()
+        Me.btnSubmit = New System.Windows.Forms.Button()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(827, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuSettings, Me.mnuConnect, Me.mnuDisconnect, Me.mnuExit})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'mnuSettings
+        '
+        Me.mnuSettings.Name = "mnuSettings"
+        Me.mnuSettings.Size = New System.Drawing.Size(152, 22)
+        Me.mnuSettings.Text = "Settings"
+        '
+        'mnuConnect
+        '
+        Me.mnuConnect.Name = "mnuConnect"
+        Me.mnuConnect.Size = New System.Drawing.Size(152, 22)
+        Me.mnuConnect.Text = "Connect"
+        '
+        'mnuDisconnect
+        '
+        Me.mnuDisconnect.Name = "mnuDisconnect"
+        Me.mnuDisconnect.Size = New System.Drawing.Size(152, 22)
+        Me.mnuDisconnect.Text = "Disconnect"
+        '
+        'mnuExit
+        '
+        Me.mnuExit.Name = "mnuExit"
+        Me.mnuExit.Size = New System.Drawing.Size(152, 22)
+        Me.mnuExit.Text = "Exit"
+        '
+        'rtxtConsole
+        '
+        Me.rtxtConsole.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.rtxtConsole.Location = New System.Drawing.Point(12, 27)
+        Me.rtxtConsole.Name = "rtxtConsole"
+        Me.rtxtConsole.Size = New System.Drawing.Size(677, 333)
+        Me.rtxtConsole.TabIndex = 1
+        Me.rtxtConsole.Text = ""
+        '
+        'lbUsers
+        '
+        Me.lbUsers.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lbUsers.FormattingEnabled = True
+        Me.lbUsers.Location = New System.Drawing.Point(695, 28)
+        Me.lbUsers.Name = "lbUsers"
+        Me.lbUsers.Size = New System.Drawing.Size(120, 355)
+        Me.lbUsers.TabIndex = 2
+        '
+        'txtConsoleInput
+        '
+        Me.txtConsoleInput.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtConsoleInput.Location = New System.Drawing.Point(12, 368)
+        Me.txtConsoleInput.Name = "txtConsoleInput"
+        Me.txtConsoleInput.Size = New System.Drawing.Size(596, 20)
+        Me.txtConsoleInput.TabIndex = 3
+        '
+        'btnSubmit
+        '
+        Me.btnSubmit.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnSubmit.Location = New System.Drawing.Point(614, 367)
+        Me.btnSubmit.Name = "btnSubmit"
+        Me.btnSubmit.Size = New System.Drawing.Size(75, 23)
+        Me.btnSubmit.TabIndex = 4
+        Me.btnSubmit.Text = "Submit"
+        Me.btnSubmit.UseVisualStyleBackColor = True
+        '
+        'frmConsole
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(827, 398)
+        Me.Controls.Add(Me.btnSubmit)
+        Me.Controls.Add(Me.txtConsoleInput)
+        Me.Controls.Add(Me.lbUsers)
+        Me.Controls.Add(Me.rtxtConsole)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "frmConsole"
+        Me.Text = "frmConsole"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents FileToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents mnuSettings As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents mnuConnect As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents mnuDisconnect As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents mnuExit As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents rtxtConsole As System.Windows.Forms.RichTextBox
+    Friend WithEvents lbUsers As System.Windows.Forms.ListBox
+    Friend WithEvents txtConsoleInput As System.Windows.Forms.TextBox
+    Friend WithEvents btnSubmit As System.Windows.Forms.Button
+End Class
